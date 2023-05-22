e8476cedf342d4459214aecbc51ba57710759a96
NanaPe <plutomario@proton.me>
2023-05-22 09:05:45 +0700

    my kata 41-43

---
+++ javascript/kata-41 Simple multiplication.js
@@ -0,0 +1,6 @@
+// DESCRIPTION:
+// This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+function simpleMultiplication(n) {
+  return n * (n % 2 ? 9 : 8);
+}
